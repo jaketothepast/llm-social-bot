8bacabb1eefe70dbbbbf67627557bce76deb710d
jaketothepast <windlejacob12@gmail.com>
2024-09-22 08:13:51 -0400

    Adding function to check local model cache and download model if needed

--- src/jaketothepast/llms/local.clj
+++ src/jaketothepast/llms/local.clj
@@ -1,13 +1,34 @@
 (ns jaketothepast.llms.local
-  (:require [jaketothepast.llms.protocols :as protocols]))
+  (:require [jaketothepast.llms.protocols :as protocols]
+            [clojure.edn :as edn]
+            [clojure.java.io :as io]))
 
 ;; This namespace encapsulates using local LLMs via llama.clj. To use these models, simply download the models
 ;; yourself, then load in the model type using the classpath.
 
-(defrecord Local [url local-dir]
+(def cache-file "models.edn")
+
+(defrecord Local [url local-dir model]
   clojure.lang.IFn
   (invoke [_ commit-msg]
     (prn commit-msg))
   protocols/PromptProto
   (make-prompt [_ message]
     ))
+
+;; TODO: Retrieve the model if the model hasn't already been downloaded
+(defn- retrieve-model [local-dir url]
+  (let [cache (slurp )]))
+
+(defn config->Local [url local-dir]
+  ;; Download the model first, and then use it in the API call
+  (let [model-weights (retrieve-model local-dir url)]))
+
+;; TODO
+;; 1. Handle local model directory
+;; 2. EDN file in model directory that shows what has been downloaded, a cache of requests
+
+(comment
+  (-> (io/file "~" "models") (.getCanonicalFile))
+  (-> (io/file "../models") (.getAbsolutePath))
+  )
